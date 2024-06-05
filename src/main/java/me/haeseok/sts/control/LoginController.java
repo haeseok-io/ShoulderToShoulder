17a7fd17270ae6b932d5b9924b7cc09b4d8cce1f
haeseok <haeseok_@naver.com>
2024-06-05 19:12:24 +0900

    로그인 컨트롤 생성

---
+++ src/main/java/me/haeseok/sts/control/LoginController.java
@@ -0,0 +1,12 @@
+package me.haeseok.sts.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
