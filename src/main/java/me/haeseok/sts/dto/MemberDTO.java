c1ec2c44ea32916f606bd417368921a93e09d923
haeseok <haeseok_@naver.com>
2024-06-10 17:02:29 +0900

    비밀번호 암호화 및 비밀번호 체크 메서드 추가

--- src/main/java/me/haeseok/sts/dto/MemberDTO.java
+++ src/main/java/me/haeseok/sts/dto/MemberDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.sql.Timestamp;
 
@@ -23,4 +24,13 @@ public class MemberDTO {
     private String blogLink;
     private Timestamp regdate;
     private String role;
+
+    public MemberDTO hashPassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+        return this;
+    }
+
+    public boolean checkPassword(String password, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(password, this.password);
+    }
 }
