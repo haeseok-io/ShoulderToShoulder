86838deef3282f1c8e0eacf7610198723228fb4f
haeseok <haeseok_@naver.com>
2024-06-10 17:02:49 +0900

    멤버 서비스 처리 생성

---
+++ src/main/java/me/haeseok/sts/service/MemberServiceImple.java
@@ -0,0 +1,44 @@
+package me.haeseok.sts.service;
+
+import lombok.RequiredArgsConstructor;
+import me.haeseok.sts.dao.MemberDAO;
+import me.haeseok.sts.dto.MemberDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberServiceImple implements MemberService {
+    private final MemberDAO memberDAO;
+    private final PasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public MemberDTO register(MemberDTO memberDTO) {
+
+        return null;
+    }
+
+    /**
+     * # 이메일 중복 체크
+     *
+     * @Param email 이메일
+     * @Return true | false
+     */
+    @Override
+    public boolean isEmailExist(String email) {
+        return memberDAO.emailExist(email);
+    }
+
+    /**
+     * # 닉네임 중복 체크
+     *
+     * @Param nickname 닉네임
+     * @Return true | false
+     */
+    @Override
+    public boolean isNicknameExist(String nickname) {
+        return memberDAO.nicknameExist(nickname);
+    }
+}
