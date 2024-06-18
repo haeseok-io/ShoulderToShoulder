55823dfe1c3e8f058d31e6b468db4bff1b3f7d9d
haeseok <haeseok_@naver.com>
2024-06-18 17:11:00 +0900

    이메일과 서비스제공자로 조회하여 회원 찾는 메서드 추가

--- src/main/java/me/haeseok/sts/dao/MemberDAO.java
+++ src/main/java/me/haeseok/sts/dao/MemberDAO.java
@@ -2,7 +2,10 @@ package me.haeseok.sts.dao;
 
 import me.haeseok.sts.dto.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 @Mapper
@@ -10,6 +13,7 @@ public interface MemberDAO {
     void addMember(MemberDTO memberDTO);
     MemberDTO findMemberNo(Long no);
     MemberDTO findMemberEmail(String email);
+    MemberDTO findMemberEmailAsProvider(@Param("email") String email, @Param("provider") String provider);
     Boolean emailExist(String email);
     Boolean nicknameExist(String nickname);
 
