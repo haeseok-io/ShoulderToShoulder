6b1a221760f8d0f508395f7e3f835f8a9279a239
haeseok <haeseok_@naver.com>
2024-06-18 17:10:22 +0900

    카카오 응답 클래스 생성

---
+++ src/main/java/me/haeseok/sts/security/dto/KakaoResponse.java
@@ -0,0 +1,35 @@
+package me.haeseok.sts.security.dto;
+
+import java.util.Map;
+
+public class KakaoResponse implements OAuth2Response {
+    private final Map<String, Object> attribute;
+    private Map<String, Object> kakaoAccountAttributes;
+    private Map<String, Object> profileAttributes;
+
+    public KakaoResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+        this.kakaoAccountAttributes = (Map<String, Object>) attribute.get("kakao_account");
+        this.profileAttributes = (Map<String, Object>) kakaoAccountAttributes.get("profile");
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return kakaoAccountAttributes.get("email").toString();
+    }
+
+    @Override
+    public String getNickname() {
+        return profileAttributes.get("nickname").toString();
+    }
+}
