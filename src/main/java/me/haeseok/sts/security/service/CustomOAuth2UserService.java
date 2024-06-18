5a24d1ec32608d3bd0df0edefd1adde66dd98035
haeseok <haeseok_@naver.com>
2024-06-18 18:00:00 +0900

    네이버 로그인 추가

--- src/main/java/me/haeseok/sts/security/service/CustomOAuth2UserService.java
+++ src/main/java/me/haeseok/sts/security/service/CustomOAuth2UserService.java
@@ -5,6 +5,7 @@ import me.haeseok.sts.dao.MemberDAO;
 import me.haeseok.sts.dto.MemberDTO;
 import me.haeseok.sts.security.CustomOAuth2User;
 import me.haeseok.sts.security.dto.KakaoResponse;
+import me.haeseok.sts.security.dto.NaverResponse;
 import me.haeseok.sts.security.dto.OAuth2Response;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -26,6 +27,8 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2Response oAuth2Response = null;
         if( serviceProvider.equals("kakao") ) {
             oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
+        } else if( serviceProvider.equals("naver") ) {
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
         } else {
             return null;
         }
