fd04097f5c23cfaedd391e8efaef7f99c5af10b9
haeseok <haeseok_@naver.com>
2024-06-19 17:09:39 +0900

    모일 개설 컨트롤 추가

--- src/main/java/me/haeseok/sts/control/MoimController.java
+++ src/main/java/me/haeseok/sts/control/MoimController.java
@@ -1,15 +1,27 @@
 package me.haeseok.sts.control;
 
+import lombok.RequiredArgsConstructor;
+import me.haeseok.sts.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/moim")
+@RequiredArgsConstructor
 public class MoimController {
+    private final CategoryService categoryService;
+
     @GetMapping("/")
     public String moimList() {
-
         return "moim/list";
+    }
+
+    @GetMapping("/write")
+    public String write0(Model model) {
+        model.addAttribute("categoryList", categoryService.readAll());
+
+        return "moim/write";
     }
 }
