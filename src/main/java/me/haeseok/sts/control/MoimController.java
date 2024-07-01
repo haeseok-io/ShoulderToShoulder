d7b5dfdee4f3a05fb3277d0b169b2f020b296171
haeseok <haeseok_@naver.com>
2024-07-01 16:20:38 +0900

    카테고리, 플랫폼, 온라인, 직무 등 모임 등록시 필요한 데이터를 가져오는 로직 추가

--- src/main/java/me/haeseok/sts/control/MoimController.java
+++ src/main/java/me/haeseok/sts/control/MoimController.java
@@ -2,6 +2,9 @@ package me.haeseok.sts.control;
 
 import lombok.RequiredArgsConstructor;
 import me.haeseok.sts.service.CategoryService;
+import me.haeseok.sts.service.OnlineService;
+import me.haeseok.sts.service.PlatformService;
+import me.haeseok.sts.service.PositionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MoimController {
     private final CategoryService categoryService;
+    private final PlatformService platformService;
+    private final OnlineService onlineService;
+    private final PositionService positionService;
 
     @GetMapping("/")
     public String moimList() {
@@ -20,7 +26,10 @@ public class MoimController {
 
     @GetMapping("/write")
     public String write0(Model model) {
-        model.addAttribute("categoryList", categoryService.readAll());
+        model.addAttribute("categoryList", categoryService.readCategoryList());
+        model.addAttribute("platformList", platformService.readPlatformList());
+        model.addAttribute("onlineList", onlineService.readOnlineList());
+        model.addAttribute("positionList", positionService.readPositionList());
 
         return "moim/write";
     }
