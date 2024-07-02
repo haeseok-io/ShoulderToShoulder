e728663d9caffd6dba6f5c3d203824bcd6e5e83e
haeseok <haeseok_@naver.com>
2024-07-02 13:11:31 +0900

    스터디 카테고리 서비스 추가 등록 메서드 테스트

--- src/main/java/me/haeseok/sts/control/MoimController.java
+++ src/main/java/me/haeseok/sts/control/MoimController.java
@@ -1,13 +1,13 @@
 package me.haeseok.sts.control;
 
 import lombok.RequiredArgsConstructor;
-import me.haeseok.sts.service.CategoryService;
-import me.haeseok.sts.service.OnlineService;
-import me.haeseok.sts.service.PlatformService;
-import me.haeseok.sts.service.PositionService;
+import me.haeseok.sts.request.MoimWriteRequest;
+import me.haeseok.sts.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MoimController {
     private final CategoryService categoryService;
+    private final StudyCategoryService studyCategoryService;
     private final PlatformService platformService;
     private final OnlineService onlineService;
     private final PositionService positionService;
@@ -25,12 +26,19 @@ public class MoimController {
     }
 
     @GetMapping("/write")
-    public String write0(Model model) {
+    public String write(Model model) {
         model.addAttribute("categoryList", categoryService.readCategoryList());
+        model.addAttribute("studyCategoryList", studyCategoryService.readStudyCategoryList());
         model.addAttribute("platformList", platformService.readPlatformList());
         model.addAttribute("onlineList", onlineService.readOnlineList());
         model.addAttribute("positionList", positionService.readPositionList());
 
         return "moim/write";
+    }
+
+    @PostMapping("/write")
+    public String writeProcess(@ModelAttribute MoimWriteRequest request) {
+        System.out.println(request);
+        return "redirect:/";
     }
 }
