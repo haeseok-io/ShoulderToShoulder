d7bb208ade43863c2b6dc73b22dee7bb502c3c6d
haeseok <haeseok_@naver.com>
2024-07-04 14:50:54 +0900

    모임 참고링크 DTO 생성

---
+++ src/main/java/me/haeseok/sts/dto/MoimLinkDTO.java
@@ -0,0 +1,16 @@
+package me.haeseok.sts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoimLinkDTO {
+    private Long no;
+    private String url;
+    private Long moimNo;
+}
