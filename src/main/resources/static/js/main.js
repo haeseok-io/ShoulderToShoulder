c9e033b0284281b2a256bf2ac060bad9c64d3cbc
haeseok <haeseok_@naver.com>
2024-07-10 13:40:52 +0900

    multiple select 선택 옵션 가져오기 함수 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -24,3 +24,34 @@ const getPositionDetail = (positionTarget, positionDetailTarget, placeholder) =>
         }, "json");
     }
 }
+
+
+// multiple select 선택 옵션 가져오기
+const getMultipleValues = name => {
+    let valueArray = [];
+    let element = $(`[name='${name}']`);
+
+    if( element.get(0).localName==="select" ) {
+        element.find("option").each((idx, el) => {
+            if( $(el).is(":selected") ) {
+                valueArray.push($(el).val());
+            }
+        });
+    } else {
+        let elementType = element.attr("type");
+
+        if( elementType==='checkbox' ) {
+            element.each((idx, el) => {
+                if( $(el).is(":checked") ) {
+                    valueArray.push($(el).val());
+                }
+            });
+        } else {
+            element.each((idx, el) => {
+                valueArray.push($(el).val());
+            });
+        }
+    }
+
+    return valueArray;
+}
