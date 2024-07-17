48ead810d4df7a0e077e680f30ffcc797fe0816c
haeseok <haeseok_@naver.com>
2024-07-17 15:05:30 +0900

    가져온 모임리스트를 받아오기 위한 응답 클래스

---
+++ src/main/java/me/haeseok/sts/response/MoimListResponse.java
@@ -0,0 +1,48 @@
+package me.haeseok.sts.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.haeseok.sts.dto.MemberDTO;
+import me.haeseok.sts.dto.MoimDTO;
+import me.haeseok.sts.dto.MoimHeadcountDTO;
+import me.haeseok.sts.dto.MoimLanguageDTO;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoimListResponse {
+    private Long no;
+    private String type;
+    private String subject;
+    private String explanation;
+    private String thumbnail;
+    private String status;
+    private int like;
+    private int hits;
+    private Timestamp regdate;
+
+    private MemberDTO writer;
+    private Object category;
+    private List<MoimLanguageDTO> languageList;
+    private List<MoimHeadcountDTO> headcountList;
+
+    public static MoimListResponse convertMoimDTO(MoimDTO moimDTO) {
+        return MoimListResponse.builder()
+                .no(moimDTO.getNo())
+                .type(moimDTO.getType())
+                .subject(moimDTO.getSubject())
+                .explanation(moimDTO.getExplanation())
+                .thumbnail(moimDTO.getThumbnail())
+                .status(moimDTO.getStatus())
+                .like(moimDTO.getLike())
+                .hits(moimDTO.getHits())
+                .regdate(moimDTO.getRegdate())
+                .build();
+    }
+}
