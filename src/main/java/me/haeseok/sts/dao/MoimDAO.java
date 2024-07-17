0d35819e8de367420fde086c0b314130345a917b
haeseok <haeseok_@naver.com>
2024-07-17 15:04:54 +0900

    모임 전체 갯수와 검색된 리스트를 가져오는 메서드 추가

--- src/main/java/me/haeseok/sts/dao/MoimDAO.java
+++ src/main/java/me/haeseok/sts/dao/MoimDAO.java
@@ -1,11 +1,16 @@
 package me.haeseok.sts.dao;
 
 import me.haeseok.sts.dto.MoimDTO;
+import me.haeseok.sts.request.MoimListRequest;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface MoimDAO {
     void addOne(MoimDTO dto);
+    int readSearchTotal(MoimListRequest request);
+    List<MoimDTO> readSearchList(MoimListRequest request);
 }
