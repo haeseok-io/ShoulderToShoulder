f561ff844093553682e7b38089811445bda1f7e3
haeseok <haeseok_@naver.com>
2024-07-18 15:47:03 +0900

    모임 번호로 모집인원 데이터 가져오는 매퍼 추가

--- src/main/java/me/haeseok/sts/dao/MoimHeadcountDAO.java
+++ src/main/java/me/haeseok/sts/dao/MoimHeadcountDAO.java
@@ -4,8 +4,11 @@ import me.haeseok.sts.dto.MoimHeadcountDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Mapper
 @Repository
 public interface MoimHeadcountDAO {
     void addOne(MoimHeadcountDTO dto);
+    List<MoimHeadcountDTO> getMoimHeadcountByMoimNo(Long moimNo);
 }
