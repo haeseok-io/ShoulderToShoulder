b755355133d66073d191d673e2db9fa077f4d3a9
haeseok <haeseok_@naver.com>
2024-07-18 15:48:03 +0900

    모임 번호로 모임 사용언어 가져오기 추가

--- src/main/java/me/haeseok/sts/dao/MoimLanguageDAO.java
+++ src/main/java/me/haeseok/sts/dao/MoimLanguageDAO.java
@@ -4,8 +4,11 @@ import me.haeseok.sts.dto.MoimLanguageDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Mapper
 @Repository
 public interface MoimLanguageDAO {
     void addOne(MoimLanguageDTO dto);
+    List<MoimLanguageDTO> getMoimLanguageByMoimNo(Long moimNo);
 }
