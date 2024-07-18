9476a97b891488bdd9f52ca805ddf03af0cfb4f0
haeseok <haeseok_@naver.com>
2024-07-18 15:52:01 +0900

    메서드명 변경

--- src/main/java/me/haeseok/sts/service/StudyCategoryService.java
+++ src/main/java/me/haeseok/sts/service/StudyCategoryService.java
@@ -1,10 +1,9 @@
 package me.haeseok.sts.service;
 
-import me.haeseok.sts.dto.CategoryDTO;
 import me.haeseok.sts.dto.StudyCategoryDTO;
 
 import java.util.List;
 
 public interface StudyCategoryService {
-    List<StudyCategoryDTO> readStudyCategoryList();
+    List<StudyCategoryDTO> readStudyCategoryAll();
 }
