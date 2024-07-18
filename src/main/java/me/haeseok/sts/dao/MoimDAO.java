3cfcf5374442b4869f488fa244c5d407c4a4e791
haeseok <haeseok_@naver.com>
2024-07-18 15:46:18 +0900

    mapper id 변경 요청된 정보로 갯수와 데이터 가져오기 추가

--- src/main/java/me/haeseok/sts/dao/MoimDAO.java
+++ src/main/java/me/haeseok/sts/dao/MoimDAO.java
@@ -2,6 +2,7 @@ package me.haeseok.sts.dao;
 
 import me.haeseok.sts.dto.MoimDTO;
 import me.haeseok.sts.request.MoimListRequest;
+import me.haeseok.sts.response.MoimListResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,9 @@ import java.util.List;
 @Mapper
 @Repository
 public interface MoimDAO {
+    Long getTotal();
+    Long getSearchTotal(MoimListRequest request);
+    List<MoimDTO> findSearchList(MoimListRequest request);
+
     void addOne(MoimDTO dto);
-    int readSearchTotal(MoimListRequest request);
-    List<MoimDTO> readSearchList(MoimListRequest request);
 }
