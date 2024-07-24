b19a82a46e0abc839ed73dbb8756b0e0fe2b6f94
haeseok <haeseok_@naver.com>
2024-07-24 21:47:11 +0900

    회원 리스트를 가져오는 서비스 추가

--- src/main/java/me/haeseok/sts/service/MemberService.java
+++ src/main/java/me/haeseok/sts/service/MemberService.java
@@ -1,9 +1,13 @@
 package me.haeseok.sts.service;
 
 import me.haeseok.sts.dto.MemberDTO;
+import me.haeseok.sts.request.MemberListRequest;
+import me.haeseok.sts.response.CustomPageResponse;
+import me.haeseok.sts.response.MemberListResponse;
 
 public interface MemberService {
     MemberDTO register(MemberDTO memberDTO);
+    CustomPageResponse<MemberListResponse> readMemberList(MemberListRequest request);
     boolean isEmailExist(String email);
     boolean isNicknameExist(String nickname);
 }
--- src/main/java/me/haeseok/sts/service/MemberServiceImple.java
+++ src/main/java/me/haeseok/sts/service/MemberServiceImple.java
@@ -2,17 +2,27 @@ package me.haeseok.sts.service;
 
 import lombok.RequiredArgsConstructor;
 import me.haeseok.sts.dao.MemberDAO;
+import me.haeseok.sts.dao.MemberDetailDAO;
 import me.haeseok.sts.dto.MemberDTO;
+import me.haeseok.sts.dto.MemberDetailDTO;
+import me.haeseok.sts.request.MemberListRequest;
+import me.haeseok.sts.response.CustomPageResponse;
+import me.haeseok.sts.response.MemberListResponse;
+import me.haeseok.sts.response.MoimListResponse;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MemberServiceImple implements MemberService {
-    private final MemberDAO memberDAO;
     private final PasswordEncoder bCryptPasswordEncoder;
+    private final MemberDAO memberDAO;
+    private final MemberDetailDAO memberDetailDAO;
 
     @Override
     public MemberDTO register(MemberDTO memberDTO) {
@@ -22,6 +32,32 @@ public class MemberServiceImple implements MemberService {
         memberDAO.addMember(memberDTO);
         
         return memberDTO;
+    }
+
+    @Override
+    public CustomPageResponse<MemberListResponse> readMemberList(MemberListRequest request) {
+        // Page
+        Pageable pageable = request.getPageRequest().getPageable(request.getSortField(), request.getSortType());
+        request.setStart(pageable.getOffset());
+        request.setEnd(pageable.getPageSize());
+
+        // Data
+        List<MemberListResponse> memberResponseList = memberDAO.findSearchList(request).stream().map(data -> {
+            MemberDetailDTO memberDetailDTO = memberDetailDAO.findMemberDetailByNo(data.getNo());
+            String profileImg = memberDetailDTO!=null ? memberDetailDTO.getProfileImg() : null;
+
+            MemberListResponse memberListResponse = MemberListResponse.convertMemberDTO(data);
+            memberListResponse.setProfileImg(profileImg);
+
+
+            return memberListResponse;
+        }).toList();
+
+        return CustomPageResponse.<MemberListResponse>pageBuilder()
+                .request(request.getPageRequest())
+                .dataList(memberResponseList)
+                .total(memberDAO.getSearchTotal(request))
+                .build();
     }
 
     /**
