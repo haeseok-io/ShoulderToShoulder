975a83c54c959cdc9c272d08d1b512b4846077bd
haeseok <haeseok_@naver.com>
2024-07-24 21:44:22 +0900

    메서드명 재정의

--- src/test/java/me/haeseok/sts/dao/MemberDAOTest.java
+++ src/test/java/me/haeseok/sts/dao/MemberDAOTest.java
@@ -38,7 +38,7 @@ class MemberDAOTest {
     @DisplayName("단일 회원 조회")
     void readMember() {
         memberDAO.addMember(memberDTO);
-        MemberDTO member = memberDAO.findMemberNo(memberDTO.getNo());
+        MemberDTO member = memberDAO.findMemberByNo(memberDTO.getNo());
         assertThat(member.getNickname()).isEqualTo("테스트");
     }
 
@@ -48,6 +48,6 @@ class MemberDAOTest {
         memberDAO.addMember(memberDTO);
         memberDAO.dropMember(memberDTO.getNo());
 
-        assertThat(memberDAO.findMemberNo(memberDTO.getNo())).isNull();
+        assertThat(memberDAO.findMemberByNo(memberDTO.getNo())).isNull();
     }
 }
