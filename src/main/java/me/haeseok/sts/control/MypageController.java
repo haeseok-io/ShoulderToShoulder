a782afe883e07fb94dfb78a9c74f238d62bd898a
haeseok <haeseok_@naver.com>
2024-07-31 19:59:28 +0900

    내정보 컨트롤에 회원정보 받아오는 서비스 추가

--- src/main/java/me/haeseok/sts/control/MypageController.java
+++ src/main/java/me/haeseok/sts/control/MypageController.java
@@ -1,7 +1,11 @@
 package me.haeseok.sts.control;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import me.haeseok.sts.response.MemberResponse;
+import me.haeseok.sts.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/mypage")
 public class MypageController {
+    private final MemberService memberService;
 
     @GetMapping(value = {"/", "/user"})
-    public String user() {
+    public String user(HttpSession session, Model model) {
+        Long memberNo = (Long) session.getAttribute("MEMBERNO");
+
+        model.addAttribute("data", memberService.readMember(memberNo));
+
         return "mypage/user";
     }
 }
