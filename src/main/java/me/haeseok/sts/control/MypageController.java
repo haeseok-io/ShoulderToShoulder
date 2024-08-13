831fa0ada316df7db083b0124d3f542aea1501ff
haeseok <haeseok_@naver.com>
2024-08-13 17:20:42 +0900

    정보수정 시 필요한 직무, 온/오프라인여부, 관심분야 추가

--- src/main/java/me/haeseok/sts/control/MypageController.java
+++ src/main/java/me/haeseok/sts/control/MypageController.java
@@ -2,8 +2,10 @@ package me.haeseok.sts.control;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import me.haeseok.sts.response.MemberResponse;
+import me.haeseok.sts.service.CategoryService;
 import me.haeseok.sts.service.MemberService;
+import me.haeseok.sts.service.OnlineService;
+import me.haeseok.sts.service.PositionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mypage")
 public class MypageController {
     private final MemberService memberService;
+    private final PositionService positionService;
+    private final OnlineService onlineService;
+    private final CategoryService categoryService;
 
     @GetMapping(value = {"/", "/user"})
     public String user(HttpSession session, Model model) {
         Long memberNo = (Long) session.getAttribute("MEMBERNO");
 
         model.addAttribute("data", memberService.readMember(memberNo));
+        model.addAttribute("positionList", positionService.readPositionList());
+        model.addAttribute("onlineList", onlineService.readOnlineList());
+        model.addAttribute("categoryList", categoryService.readCategoryAll());
 
         return "mypage/user";
     }
