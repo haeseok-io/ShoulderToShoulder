9cf895b96a575b561c3062da2a5e0d1e0a9cca9d
haeseok <haeseok_@naver.com>
2024-11-05 14:44:46 +0900

    특정 디렉토리에 저장될 수 있도록 로직 수정

--- src/main/java/me/haeseok/sts/control/ImgController.java
+++ src/main/java/me/haeseok/sts/control/ImgController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
 import java.net.URLDecoder;
@@ -16,16 +17,30 @@ import java.nio.file.Files;
 @Controller
 public class ImgController {
     @Value("${me.haeseok.sts.upload.directory.moim}")
-    private String uploadPath;
+    private String moimUploadPath;
+
+    @Value("${me.haeseok.sts.upload.directory.user}")
+    private String userUploadPath;
 
     @GetMapping("/display")
-    public ResponseEntity<byte[]> getFile(String fileName) {
+    public ResponseEntity<byte[]> getFile(String fileName, @RequestParam(required = false) String directory) {
         ResponseEntity<byte[]> result;
 
         try {
             HttpHeaders header = new HttpHeaders();
             String srcFileName = URLDecoder.decode(fileName, StandardCharsets.UTF_8);
-            File file = new File(uploadPath+File.separator + srcFileName);
+            File file = null;
+
+            if( directory==null || directory.equals("moim") ) {
+                file = new File(moimUploadPath+File.separator + srcFileName);
+            } else if( directory.equals("user") ) {
+                file = new File(userUploadPath+File.separator + srcFileName);
+            }
+
+            // 파일이 존재하는지 확인 후 처리
+            if (!file.exists()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 파일을 찾지 못하면 404 반환
+            }
 
             // Mime 타입 처리
             header.add("Content-Type", Files.probeContentType(file.toPath()));
